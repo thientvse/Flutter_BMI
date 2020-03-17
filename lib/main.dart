8f98e017891743faa96d5c6ee9651a4a95b510b6
thientvse <thientvse@gmail.com>
2020-03-17 23:33:46 +0700

    weight card

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bmi/InputPage.dart';
+import 'package:flutter_bmi/input_page/InputPage.dart';
 import 'package:flutter/services.dart';
 
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown])
       .then((_) => runApp(new MyApp()));
